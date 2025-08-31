3be782ec1468146696e0e7b8bb4701efda194ef4
Noel Thomas <noel.ed.thomas@gmail.com>
2025-09-01 11:45:24 +1200

    Slight rewording, removal of "voucher"

--- navtreedata.js
+++ navtreedata.js
@@ -48,7 +48,7 @@ var NAVTREE =
         [ "💡 Feedback and suggestions", "md_doc_pages__9__bugs.html#autotoc_md19", null ],
         [ "❓ Questions about features", "md_doc_pages__9__bugs.html#autotoc_md20", null ],
         [ "😁  Working on something cool? I'd love to hear about it! ❤", "md_doc_pages__9__bugs.html#autotoc_md21", null ],
-        [ "🎟️ Vouchers and Free Access", "md_doc_pages__9__bugs.html#autotoc_md22", null ],
+        [ "🎟️ Access Requests", "md_doc_pages__9__bugs.html#autotoc_md22", null ],
         [ "🚫 Spam & Fraud", "md_doc_pages__9__bugs.html#autotoc_md23", null ]
       ] ]
     ] ],
